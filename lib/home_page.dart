595cc4c6bfd8ae613c48d582a18b49b20caaf082
Kevyn FLORESDACOSTA <kev.floresdacosta@ecoles-epsi.net>
2024-12-04 17:48:39 +0100

    Ajout d'une page des règles et amélioration de la page d'accueil avec un nouveau design et des boutons pour démarrer le jeu

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,22 +12,72 @@ class HomeScreen extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("images/8.jpg"), // <-- BACKGROUND IMAGE
+          image: AssetImage("images/8.jpg"),
           fit: BoxFit.cover,
         ),
-      ),
+      ), // Image de fond
       child: Scaffold(
-      backgroundColor: Color.fromRGBO(0, 0, 0, 0.35),
-      body:Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => GameScreen(gameService: gameService)),
-              );
-            },
-            child: const Text("Commencer le jeu"),
+        backgroundColor: Color.fromRGBO(0, 0, 0, 0.35),//met la couleur de fond en transparent pour voir l'image de fond
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                  Text(
+                    "Where's Waldo ?",
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,// épaisseur de la police
+                      color: Colors.white,
+                    ),
+                  ),
+              const SizedBox(height: 40),// br
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GameScreen(gameService: gameService),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red[400],
+                ),
+                child: Text(
+                  "Start Games",
+                  style: TextStyle(
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 4.0,// flou
+                        color: Colors.black,// couleur de l'ombre
+                        offset: Offset(2, 2),// décalage de l'ombre
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20), // br
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red[400],
+                ),
+                child: Text(
+                  "Rules",
+                  style: TextStyle(
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 4.0,// flou
+                        color: Colors.black, // couleur de l'ombre
+                        offset: Offset(2, 2), // décalage de l'ombre
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
